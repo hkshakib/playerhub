ec589bac69c38efad11b8632463cd5b99aff9150
hkshakib <humayunkibriashakib@gmail.com>
2023-08-06 01:49:09 +0600

    feat: Display Genres on Side Panel

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
-      {data && data.map((genre) => <li key={genre.id}> {genre.name} </li>)}
-    </ul>
+    <List>
+      {data &&
+        data.map((genre) => (
+          <ListItem key={genre.id} paddingY="15px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+    </List>
   );
 };
 
