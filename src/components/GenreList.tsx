bfa2809c8574a19963d73d431243efe9facc33f7
hkshakib <humayunkibriashakib@gmail.com>
2023-08-06 18:57:16 +0600

    feat: Highlight the Selected Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,9 +11,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -33,6 +34,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
                 variant="link"
                 fontSize="lg"
                 onClick={() => onSelectGenre(genre)}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               >
                 {genre.name}
               </Button>
