02a13d8dd232433144b2337331bdc011911f04e2
hkshakib <humayunkibriashakib@gmail.com>
2023-08-07 01:38:28 +0600

    fix: Cleanup Game Card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={3}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
